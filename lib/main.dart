25edf47a8f393184e7432a20c7cbf5f08190f63d
smartcloudio <webmaster@smartcloudio.com.br>
2021-04-20 17:53:09 -0400

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:cadastro_usuario/provider/users.dart';
+import 'package:cadastro_usuario/routes/app_routes.dart';
+import 'package:cadastro_usuario/views/user_form.dart';
+import 'package:cadastro_usuario/views/user_list.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Users(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          AppRoutes.HOME: (_) => UserList(),
+          AppRoutes.USER_FORM: (_) => UserForm()
+        },
+      ),
+    );
+  }
+}
